14ee5570a2dc102f29a45dd0e1391636ffa015dc
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-11-16 20:24:34 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: 'postgres://vzcnktnmvlysrt:e3d028dca69205f2ae3ac7c1ba91121b3322552c65789c8c19cbcf0f3660f4ca@ec2-34-200-72-77.compute-1.amazonaws.com:5432/ddlc5umfbuqpj5'
+    API_ENDPOINT: 'https://agile-river-24357.herokuapp.com/api'
   }
